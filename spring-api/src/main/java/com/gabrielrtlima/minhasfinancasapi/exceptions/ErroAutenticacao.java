6ed12c44dcc128c1afbfc52168d824a6a5001adb
Gabriel Lima <gabriel.rtlima@upe.br>
2022-02-18 01:43:45 -0300

    implementando salvar e autenticar usuario

---
+++ spring-api/src/main/java/com/gabrielrtlima/minhasfinancasapi/exceptions/ErroAutenticacao.java
@@ -0,0 +1,2 @@
+package com.gabrielrtlima.minhasfinancasapi.exceptions;public class ErroAutenticacao {
+}
---
+++ spring-api/src/test/java/com/gabrielrtlima/minhasfinancasapi/services/UsuarioServiceTest.java
@@ -0,0 +1,2 @@
+package com.gabrielrtlima.minhasfinancasapi.services;public class UsuarioServiceTest {
+}
